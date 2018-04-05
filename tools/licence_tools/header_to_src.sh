62d69d055c9bf35b64424f2e5771009581a455e9
valentin-riviere <38097768+valentin-riviere@users.noreply.github.com>
2018-04-05 10:30:58 +0000

    Update header_to_src.sh

--- tools/licence_tools/header_to_src.sh
+++ tools/licence_tools/header_to_src.sh
@@ -1,3 +1,20 @@
+/*
+This file is part of QuadMorphing project
+Copyright (C) 2018 - Institute of Movement Sciences (Marseille, FRANCE)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #!/bin/bash
 
 usage="$(basename "$0") [-h] header_path dir_src... -- program to put licence header to src/headers files
